a92dc4fac188a894eb3df09741dcbb35821da6f5
GabMoreira123 <matheo200817@gmail.com>
2025-02-17 10:45:58 -0400

    condicionais

---
+++ exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Maior que 18 anos?");
+string idade = Console.ReadLine();
+if (idade == "sim" || idade == "Sim")
+{
+    Console.WriteLine($" Entrada Liberada");
+}
+else if (idade == "Não" || idade == "não")
+{
+    Console.WriteLine($"Possui autorização do responsável?");
+
+    string alternativa = Console.ReadLine();
+    if (alternativa == "sim" || alternativa == "Sim")
+    {
+        Console.WriteLine($"Entrada Liberada");
+    }
+    else if (alternativa == "Não" || alternativa == "não")
+    {
+        Console.WriteLine($"Entrada Negada");
+    }
+}
