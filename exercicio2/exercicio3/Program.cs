4a009e1ceb628e76936847918826b6c7ef51f462
GabMoreira123 <matheo200817@gmail.com>
2025-02-17 11:15:27 -0400

    exercicio3

---
+++ exercicio2/exercicio3/Program.cs
@@ -0,0 +1,15 @@
+﻿bool choveu = true; 
+bool tarde = false;
+
+if (!choveu) {
+    Console.WriteLine("Vou pedalar outro dia");
+} 
+else 
+{
+    if (tarde) {
+        Console.WriteLine("Vou pedalar outro dia.");
+    } 
+    else {
+        Console.WriteLine("Vou pedalar.");
+    }
+}
