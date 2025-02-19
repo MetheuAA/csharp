368ddd93c12f76d244dbe791ca346f48780596ab
MetheoAA <matheo200817@gmail.com>
2025-02-19 07:43:05 -0400

    add estrutura de repetição

---
+++ estrutura-repeticao/Program.cs
@@ -0,0 +1,10 @@
+﻿Random numeroAleatorio = new Random();
+int numero = numeroAleatorio.Next(1, 101);
+int contador = 0;
+Console.WriteLine($"Número Alvo: {numero}");
+while (contador <= numero)
+{
+    Console.WriteLine($"Contador:{contador}");
+    contador++;
+}
+Console.WriteLine($"Contador atingiu o número alvo: {contador}");
