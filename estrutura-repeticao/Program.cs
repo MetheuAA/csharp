c805b3f5986412a7c095be57b6b061c2b6a4c0d7
MetheoAA <matheo200817@gmail.com>
2025-02-19 07:59:50 -0400

    add do while

--- estrutura-repeticao/Program.cs
+++ estrutura-repeticao/Program.cs
@@ -8,3 +8,12 @@ while (contador <= numero)
     contador++;
 }
 Console.WriteLine($"Contador atingiu o número alvo: {contador}");
+
+// Estrutura de repetição do...while
+
+int numero1 = 0;
+
+do{
+    Console.WriteLine($"Número: {numero1}");
+    numero1++;
+} while (numero1 <10);
