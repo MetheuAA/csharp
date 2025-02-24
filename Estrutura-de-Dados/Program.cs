9487d1e4aebe2d857c841ee2274f06e5ae582a09
GabMoreira123 <matheo200817@gmail.com>
2025-02-24 08:54:21 -0400

    estrutura de dados

---
+++ Estrutura-de-Dados/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] meuArray =  { 1, 2, 3, 4, 5};
+
+
+// Loop com foreach
+foreach (int i in meuArray)
+{
+    Console.WriteLine(i);
+}
+
+// Lista
+
+List<string> listaNomes = new List<string>{"Wanderson", "Jaene", "Ryan"};
+listaNomes.Add("Maria");
+foreach (string i in listaNomes)
+{
+    Console.WriteLine(i);
+}
+listaNomes.Remove("Jaene");
+foreach (string i in listaNomes)
+{
+    Console.WriteLine(i);
+}
+
+//Hash Set
+
+HashSet<int> conjunto = new HashSet<int> { 1, 2, 3};
+conjunto.Add(4);
+
+foreach (int i in conjunto)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("");
+
+conjunto.Add(2);
+
+foreach (int i in conjunto)
+{
+    Console.WriteLine(i);
+}
---
+++ exercicio-estrutura-de-dados/Program.cs
@@ -0,0 +1,10 @@
+﻿// exercicio 1 (somar todos os elementos do array e exiba o resultado no console)
+
+int[] array = { 1, 2, 3, 4, 5 };
+int sum = 0;
+
+foreach (int num in array)
+{
+    sum += num;
+}
+Console.WriteLine(sum);
