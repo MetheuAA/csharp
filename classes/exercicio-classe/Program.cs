ff620402b46aaab4cd8fdc8477679473772c5714
GabMoreira123 <matheo200817@gmail.com>
2025-03-10 09:55:32 -0400

    Add exercicio de classe

---
+++ classes/exercicio-classe/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using exercicio_classe.model;
+
+Veiculo obj = new Veiculo ("Honda", "Civic", 2020);
+obj.Acelerar();
+obj.Freiar();
--- classes/exercicio-classe/obj/Debug/net9.0/exercicio-classe.AssemblyInfo.cs
+++ classes/exercicio-classe/obj/Debug/net9.0/exercicio-classe.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("classes")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("exercicio-classe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+076f709397d341bcd541087b6f7244fbd45af244")]
-[assembly: System.Reflection.AssemblyProductAttribute("classes")]
-[assembly: System.Reflection.AssemblyTitleAttribute("classes")]
+[assembly: System.Reflection.AssemblyProductAttribute("exercicio-classe")]
+[assembly: System.Reflection.AssemblyTitleAttribute("exercicio-classe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
