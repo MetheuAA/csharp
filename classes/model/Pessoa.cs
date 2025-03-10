519be693cd7ac6098569fa0715e8180da12634b2
GabMoreira123 <matheo200817@gmail.com>
2025-03-10 07:56:06 -0400

    add classe pessoa

---
+++ classes/model/Pessoa.cs
@@ -0,0 +1,17 @@
+public class Pessoa
+{
+    // Atributos
+    string nome;
+    int idade;
+    double altura;
+    double peso;
+
+    //Construtor
+    public Pessoa(string nome, int idade, double altura, double peso)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
