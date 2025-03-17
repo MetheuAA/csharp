5ba636bed8b111ef209f5d2427e6cc25b1dbfd9d
GabMoreira123 <matheo200817@gmail.com>
2025-03-17 10:00:14 -0400

    add implementação do métodos obter dados do veiculo na classe moto

--- POO/model/Moto.cs
+++ POO/model/Moto.cs
@@ -8,8 +8,8 @@ namespace POO.model
 {
     public class Moto: Veiculo
     {
-        private bool partidaEletrica{get; set;}
-        private bool protetorDeCarenagem{get; set;}
+        private bool partidaEletrica
+        private bool protetorDeCarenagem
 
         public Moto(string marca, string modelo, int ano, double velocidadeAtual, bool partidaEletrica, bool protetorDeCarenagem) : base(marca, modelo, ano, velocidadeAtual)
         {
@@ -43,6 +43,15 @@ namespace POO.model
         public void Empinar()
         {
             Console.WriteLine("Empinando a moto!");
+        }
+        public override void ObterDadosVeiculo()
+        {
+            Console.WriteLine($"\nDados da moto:\n");
+            Console.WriteLine($"Marca: {GetMarca()}");
+            Console.WriteLine($"Modelo: {GetModelo()}");
+            Console.WriteLine($"Ano: {GetAno()}");
+            Console.WriteLine($"Partida elétrica: {(GetPartidaEletrica() ? "Sim" : "Não")}");
+            Console.WriteLine($"Protetor de carenagem: {(GetProtetorDeCarenagem() ? "Sim" : "Não")}");
         }
     }
 }
